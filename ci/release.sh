e2bbdcf2e9a316dc0735a91daf8e34a103cba004
dshah614 <dipen_shah2@cable.comcast.com>
2022-06-08 06:58:19 -0400

    Add semantic release

---
+++ ci/release.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd source-code
+
+echo "init-author-name=Dipen" >> .npmrc
+echo "init-author-email=dipenvshah@gmail.com" >> .npmrc
+echo "//registry.npmjs.org/:_authToken=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" >> .npmrc
+echo "init-license=MIT" >> .npmrc
+npm run semantic-release -- --no-ci
