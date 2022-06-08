63109f0eb48f99741c4497f9754044c17790a336
dshah614 <dipen_shah2@cable.comcast.com>
2022-06-08 15:22:50 -0400

    fix(package): update package

--- ci/release.sh
+++ ci/release.sh
@@ -6,8 +6,9 @@ ssh-keyscan -t rsa -H github.com >> ~/.ssh/known_hosts
 
 cd dipencode
 npm ci
+echo "GH_TOKEN::${GH_TOKEN}"
 echo "init-author-name=Dipen" >> .npmrc
 echo "init-author-email=dipenvshah@gmail.com" >> .npmrc
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
 echo "init-license=MIT" >> .npmrc
-npm run semantic-release -- --no-ci
+GH_TOKEN=${GH_TOKEN} npm run semantic-release -- --no-ci
