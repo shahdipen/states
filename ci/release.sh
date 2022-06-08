7c73c713de0def65fba10d21e62ff171badf668c
dshah614 <dipen_shah2@cable.comcast.com>
2022-06-08 10:11:34 -0400

    fix(semantic-release): fix semantic release

--- ci/release.sh
+++ ci/release.sh
@@ -4,6 +4,6 @@ cd dipencode
 npm ci 
 echo "init-author-name=Dipen" >> .npmrc
 echo "init-author-email=dipenvshah@gmail.com" >> .npmrc
-echo "//registry.npmjs.org/:_authToken=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" >> .npmrc
+echo "//registry.npmjs.org/:_authToken=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" >> .npmrc
 echo "init-license=MIT" >> .npmrc
 npm run semantic-release -- --no-ci
