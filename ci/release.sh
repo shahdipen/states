cb54de63c3972b8957c82eea821817c0a299200b
dshah614 <dipen_shah2@cable.comcast.com>
2022-06-08 12:10:02 -0400

    update package version

--- ci/release.sh
+++ ci/release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd dipencode
-npm ci 
+npm ci
 echo "init-author-name=Dipen" >> .npmrc
 echo "init-author-email=dipenvshah@gmail.com" >> .npmrc
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
