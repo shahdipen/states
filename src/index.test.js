d30b15418535145551d958f43ddceb56457ad5d3
dipenvshah@gmail.com <dipenvshah@gmail.com>
2022-06-28 10:41:03 -0400

    removve test cases

--- src/index.test.js
+++ src/index.test.js
@@ -18,9 +18,4 @@ describe("random", () => {
     const randomItem = stats.random();
     expect(stats.all).to.include(randomItem);
   })
-
-  it("check array items", () => {
-    const randomItem = stats.random(3);
-    expect(randomItem).to.have.length(3);
-  })
 })
